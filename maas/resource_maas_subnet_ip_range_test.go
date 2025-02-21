c230e90268900160940440828088ca1fe3b145f9
Toby de Mendonça <toby.demendonca@canonical.com>
2025-02-21 11:29:11 +0000

    test: add test import

--- maas/resource_maas_subnet_ip_range_test.go
+++ maas/resource_maas_subnet_ip_range_test.go
@@ -19,7 +19,7 @@ func TestAccResourceMaasSubnetIPRange_basic(t *testing.T) {
 	var ipRange entity.IPRange
 	
 	subnet_name := acctest.RandomWithPrefix("test-subnet")
-
+	ip_range_attr := "maas_subnet_ip_range.test_ip_range"
 	range_type := "reserved"
 	comment := "test-comment"
 	start_ip := "10.88.88.1"
@@ -27,13 +27,12 @@ func TestAccResourceMaasSubnetIPRange_basic(t *testing.T) {
 
 	pre_check := func() { testutils.PreCheck(t, nil) }
 
-	// Test create
 	checks := []resource.TestCheckFunc{
-		testAccMAASSubnetIPRangeCheckExists("maas_subnet_ip_range.test_ip_range", &ipRange),
-		resource.TestCheckResourceAttr("maas_subnet_ip_range.test_ip_range", "type", range_type),
-		resource.TestCheckResourceAttr("maas_subnet_ip_range.test_ip_range", "comment", comment),
-		resource.TestCheckResourceAttr("maas_subnet_ip_range.test_ip_range", "start_ip", start_ip),
-		resource.TestCheckResourceAttr("maas_subnet_ip_range.test_ip_range", "end_ip", end_ip),
+		testAccMAASSubnetIPRangeCheckExists(ip_range_attr, &ipRange),
+		resource.TestCheckResourceAttr(ip_range_attr, "type", range_type),
+		resource.TestCheckResourceAttr(ip_range_attr, "comment", comment),
+		resource.TestCheckResourceAttr(ip_range_attr, "start_ip", start_ip),
+		resource.TestCheckResourceAttr(ip_range_attr, "end_ip", end_ip),
 	}
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -42,9 +41,16 @@ func TestAccResourceMaasSubnetIPRange_basic(t *testing.T) {
 		CheckDestroy: testAccCheckMAASSubnetIPRangeDestroy,
 		ErrorCheck:   func(err error) error { return err },
 		Steps: []resource.TestStep{
+			// Test create
 			{
 				Config: testAccSubnetIPRangeExampleResource(subnet_name, range_type, comment, start_ip, end_ip),
 				Check:  resource.ComposeTestCheckFunc(checks...),
+			},
+			// Test import
+			{
+				ResourceName:      ip_range_attr,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -77,6 +83,9 @@ func testAccMAASSubnetIPRangeCheckExists(rn string, ipRange *entity.IPRange) res
 	}
 }
 
+// func testAccMAASIPRangeCheckStateUpToDate(
+
+// )
 // An example resource configuration for a subnet IP range.
 // Note that a subnet is required to create an IP range.
 func testAccSubnetIPRangeExampleResource(
