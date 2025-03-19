4325f5d35b6c96bde4e5ad102bc56841128eea86
Toby de Mendonça <toby.demendonca@canonical.com>
2025-03-19 10:46:37 +0000

    chore: add simple tag read

--- maas/resource_maas_simple_tag.go
+++ maas/resource_maas_simple_tag.go
@@ -2,8 +2,8 @@ package maas
 
 import (
 	"context"
-	// "fmt"
-	// "slices"
+	"fmt"
+	"slices"
 
 	// "github.com/canonical/gomaasclient/client"
 	// "github.com/canonical/gomaasclient/entity"
@@ -24,38 +24,28 @@ func resourceMaasInterfaceTag() *schema.Resource {
 			},
 		},
 		Schema: map[string]*schema.Schema{
-			"interfaces": {
-				Type:        schema.TypeSet,
-				Optional:    true,
-				Description: "List of MAAS machine or device interfaces that will be tagged with the new tag.",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"device": {
-							Type:         schema.TypeString,
-							Optional:     true,
-							ExactlyOneOf: []string{"machine", "device"},
-							Description:  "The identifier (system ID, hostname, or FQDN) of the device with the network interface. Either `machine` or `device` must be provided.",
-						},
-						"machine": {
-							Type:         schema.TypeString,
-							Optional:     true,
-							ExactlyOneOf: []string{"machine", "device"},
-							Description:  "The identifier (system ID, hostname, or FQDN) of the machine with the network interface. Either `machine` or `device` must be provided.",
-						},
-						"ids": {
-							Type:        schema.TypeList,
-							Required:    true,
-							Description: "The network interface IDs to tag.",
-							Elem:        &schema.Schema{Type: schema.TypeInt},
-						},
-					},
-				},
+			"device": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ExactlyOneOf: []string{"machine", "device"},
+				Description:  "The identifier (system ID, hostname, or FQDN) of the device with the network interface. Either `machine` or `device` must be provided.",
 			},
-			"name": {
-				Type:        schema.TypeString,
+			"machine": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ExactlyOneOf: []string{"machine", "device"},
+				Description:  "The identifier (system ID, hostname, or FQDN) of the machine with the network interface. Either `machine` or `device` must be provided.",
+			},
+			"interface_id": {
+				Type:        schema.TypeInt,
 				Required:    true,
-				ForceNew:    true,
-				Description: "The new tag name. Because the name will be used in urls, it should be short.",
+				Description: "The network interface ID to tag.",
+			},
+			"tags": {
+				Type:        schema.TypeSet,
+				Required:    true,
+				Description: "The tags to assign to the network interface. It should be short and without spaces.",
+				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
 		},
 	}
@@ -63,27 +53,40 @@ func resourceMaasInterfaceTag() *schema.Resource {
 
 func resourceInterfaceTagCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*ClientConfig).Client
-	
-	// get relevant system id from inputs.
-	interfaces := d.Get("interfaces").(*schema.Set)
 
-	for _, iface := range interfaces.List() {
-		iface := iface.(map[string]interface{})
-		systemId, err := getSystemIDFromInterfaceMap(client, iface)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-		// Add tag for each interface
-		for _, id := range iface["ids"].([]int) {
-			client.NetworkInterface.AddTag(systemId, id, d.Get("name").(string))
-		}
+	interfaceId := d.Get("interface_id ").(int)
+	desiredTags := convertToStringSlice(d.Get("tags").(*schema.Set).List())
+	systemId, err := getMachineOrDeviceSystemID(client, d)
+	if err != nil {
+		return diag.FromErr(err)
 	}
 
-	// update state
-	name := d.Get("name").(string)
-	
-	d.SetId(name)
-	return resourceSimpleTagUpdate(ctx, d, meta)
+	// Get the existing interface
+	existingInterface, err := client.NetworkInterface.Get(systemId, interfaceId)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// Remove tags that are not in the desired set
+	if existingInterface.Tags == nil {
+		existingInterface.Tags = []string{}
+	}
+	for _, tag := range existingInterface.Tags {
+		if !slices.Contains(desiredTags, tag) {
+			client.NetworkInterface.RemoveTag(systemId, interfaceId, tag)
+		} 
+	}
+
+	// Add tags that are in the desired set. AddTag will not add duplicates.
+	for _, tag := range desiredTags {
+		client.NetworkInterface.AddTag(systemId, interfaceId, tag)
+	}
+
+	// Create the resource ID in state. A unique resource for every interface.
+	d.SetId(fmt.Sprintf("%v:%v", systemId, interfaceId))
+
+	// Read the resource to update state
+	return resourceInterfaceTagRead(ctx, d, meta)
 }
 
 func resourceInterfaceTagRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -92,8 +95,6 @@ func resourceInterfaceTagRead(ctx context.Context, d *schema.ResourceData, meta 
 }
 
 func resourceInterfaceTagUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	client := meta.(*ClientConfig).Client
-	
 	// Add tags if not present
 	// Since name is ForceNew, there's nothing to update in our dummy implementation
 	return nil
