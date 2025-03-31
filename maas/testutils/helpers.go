1312281426791ce8e6dff76b8b69f6e2d97dcd21
Toby de Mendonça <toby.demendonca@canonical.com>
2025-03-31 18:57:02 +0100

    chore: added GenerateRandomCidr function and test

--- maas/testutils/helpers.go
+++ maas/testutils/helpers.go
@@ -1,8 +1,10 @@
 package testutils
 
 import (
-	"crypto/rand"
+	"math/rand"
 	"fmt"
+	"time"
+	"log"
 )
 
 // RandomMAC generates a random locally administered MAC address.
@@ -18,4 +20,23 @@ func RandomMAC() string {
 	mac[0] = (mac[0] & 0xFE) | 0x02
 
 	return fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])
+}
+
+// Generate a random CIDR of the form 10.x.y.0/24, where x and y are random numbers in the usable range of 50 to 255
+func GenerateRandomCidr() string {
+	// Create and log a seed if required for test reproducibility
+	seed := time.Now().UnixNano()
+	rand.New(rand.NewSource(seed))
+	log.Printf("Seed used for random CIDR: %d", seed)
+
+	// Arbitrary minIpRange to ensure the CIDR generated doesn't conflict with any existing MAAS networks
+	minIpRange := 50
+	maxIpRange := 255
+	
+	cidr := fmt.Sprintf("10.%d.%d.0/24", generateRandomNumberInRange(minIpRange, maxIpRange) , generateRandomNumberInRange(minIpRange, maxIpRange))
+	return cidr
+}
+
+func generateRandomNumberInRange(min int, max int) int {
+	return rand.Intn(max-min) + min
 }
